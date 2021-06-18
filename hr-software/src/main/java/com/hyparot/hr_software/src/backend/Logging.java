218fb93fef1421baca49506b003199c19d07bba9
er <er@DEBERN0001.kpc.de>
2021-06-18 09:56:36 +0200

    actlog fertiggestellt

--- hr-software/src/main/java/com/hyparot/hr_software/src/backend/Logging.java
+++ hr-software/src/main/java/com/hyparot/hr_software/src/backend/Logging.java
@@ -1,12 +1,14 @@
 package com.hyparot.hr_software.src.backend;
 
 import java.io.File;
-import java.util.*;
+import java.io.FileWriter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Logging {
 	
 	public static void main (String[] args) {
-		writeActlog("AU001", 1234);
+		writeActlog("AZT001", 1234);
 	}
 	public static void writeActlog(String event, int persNr) {
 		try {
@@ -24,38 +26,56 @@ public class Logging {
 			
 			String massage = null;
 			
+			//AUS*** Meldungen für Useraktivitäten
+			//AVC*** Meldungen für Urlaub
+			//ASK*** Meldungen für Krankmeldungen 
+			
 			switch (event) {
-				case "AU001": 
-					massage = today + event + "Ein neuer Benutzer mit der Personalnummer "+persNr+" wurde angelegt";
+				// Meldungen mit Aktivitäten
+				case "AUS001": 
+					massage = today + event + " Ein neuer Benutzer mit der Personalnummer "+persNr+" wurde angelegt";
 					break;
-				case "AU002":
-					massage = today + event + "Der Benutzer mit der Personalnummer "+persNr+" wurde bearbeitet";
+				case "AUS002":
+					massage = today + event + " Der Benutzer mit der Personalnummer "+persNr+" wurde bearbeitet";
 					break;
-				case "AU003":
-					massage = today + event + "Der Benutzer mit der Personalnummer "+persNr+" wurde aus dem System entfernt";
+				case "AUS003":
+					massage = today + event + " Der Benutzer mit der Personalnummer "+persNr+" wurde aus dem System entfernt";
 					break;
-				case "AV001":
-					massage = today + event + "Der Benutzer mit der Personalnummer "+persNr+" hat einen neuen Urlaubsantrag gestellt";
+				case "AVC001":
+					massage = today + event + " Der Benutzer mit der Personalnummer "+persNr+" hat einen neuen Urlaubsantrag gestellt";
 					break;
-				case "AV002":
-					massage = today + event + "Der Urlaubsantrag des Benutzers mit der Personalnummer "+persNr+" wurde erfolgreich an den Vorgesetzten versendet";
+				case "AVC002":
+					massage = today + event + " Der Urlaubsantrag des Benutzers mit der Personalnummer "+persNr+" wurde an den Vorgesetzten versendet";
 					break;
-				case "AV003":
-					massage = today + event + "Der Urlaubsantrag des Benutzers mit der Personalnummer "+persNr+" wurde vom Vorgesetzen genemigt";
+				case "AVC003":
+					massage = today + event + " Der Urlaubsantrag des Benutzers mit der Personalnummer "+persNr+" wurde vom Vorgesetzen genemigt";
 					break;
-				case "AV004":
-					massage = today + event + "Der Urlaubsantrag des Benutzers mit der Personalnummer "+persNr+" wurde vom Vorgesetzen abgelehnt";
+				case "AVC004":
+					massage = today + event + " Der Urlaubsantrag des Benutzers mit der Personalnummer "+persNr+" wurde vom Vorgesetzen abgelehnt";
 					break;
+				case "ASK001":
+					massage = today + event + " Der Benutzer mit der Personalnummer "+persNr+" hat eine Krankmeldung eingereicht";
+					break;
+				case "AZT001":
+					massage = today + event + " Der Benutzer mit der Personalnummer "+persNr+" hat eine Arbeitszeit erfasst";
 				
-				
-				case "ED001":
-					massage = today + event + "Es konnte keine Verbindung zur Datenbank hergestellt werden";
+				// Meldungen mit Fehlern
+				case "EDB001":
+					massage = today + event + " Der Benutzer mit der Personalnummer "+persNr+" konnte keine Verbindung zur Datenbank aufbauen";
 					break;
-				case "ED002":
-					massage = today + event + "Die SQL Syntax hat einen Fehler";
+				case "EDB002":
+					massage = today + event + " Bei der Eingabe des Benutzers mit der Personalnummer "+persNr+" war ungültig mit der SQL Syntax";
 					break;
-				
+				case "EDB003":
+					massage = today + event + " Bei der Verbindung mit der Datenbank des Nutzers "+persNr+ "gab es einen Fehler";
+					break;
+						
 			}
+			FileWriter actlog = new FileWriter("actlog.txt", true);
+			actlog.write(massage+"\n");
+			actlog.close();
+			
+			
 			
 		} catch (Exception e) {
 			System.out.println("UPS... da ist wohl was falsch gelaufen");
